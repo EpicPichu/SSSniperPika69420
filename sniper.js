90302efd3673ea656a899c6ded0ecf72c1771ca3
EpicPichu <sakibhasan8024@gmail.com>
2024-02-27 16:24:22 +0600

    Major update

--- sniper.js
+++ sniper.js
@@ -1,36 +1,8 @@
-const { spawn } = require('child_process');
-const { exec } = require('child_process');
-const path = require('path');
-
-
-///////////////////////// Python run function
-
-function runPythonScript(scriptPath, args) {
-    return new Promise((resolve, reject) => {
-        const pythonProcess = spawn('python', [scriptPath, ...args]);
-        pythonProcess.stdout.on('data', (data) => {
-        });
-        pythonProcess.stderr.on('data', (data) => {
-            console.error(`Error executing Python script:\n${data}`);
-            reject(data.toString());
-        });
-        pythonProcess.on('close', (code) => {
-            if (code === 0) {
-                resolve();
-            } else {
-            }
-        });
-    });
-}
-
 require('dotenv').config();
 const token = process.env['token']
 
 const { Client, Message, MessageAttachment } = require('discord.js-selfbot-v13');
 const client = new Client();
-
-const prefix = 'ep-';
-const EpicPichu = '598085365815050241';
 
 
 client.on('ready', async () => {
@@ -38,70 +10,86 @@ client.on('ready', async () => {
 })
 
 
+
+/////////////////////// functions ////////////////////
+
+function mathfc(str) {
+  const p1 = str.substring(4, str.length - 2)
+  let result;
+
+  try {
+    result = eval(p1);
+  } catch (error) {
+    console.error('Error:', error);
+    result = error;
+  }
+
+  return result;
+}
+
+
+function retypefc(str) {
+  const p1 = str.substring(4, str.length - 2)
+  return p1.replace(/\s(?=\w)|(?<=\w)\s/g, '').replace(/ {2,}/g, ' ');
+}
+
+////////////////////// constants ////////////////////////////
+
+const prefix = 'ep-';
+const EpicPichu = '598085365815050241';
+const types = ['Math event', 'Retype event', 'Unscramble event', 'Trivia event'];
+const mathl1 = 'You have **100** seconds to solve the given riddle:';
+const retypel1 = "seconds to retype the following sentence:";
+const nump = /\d+/;
+
+
 //////////////////////////// chat event sniper //////////////////////////////////////
 
 client.on('messageCreate', (message) => {
-  // Check if the message contains any embeds
   if (message.embeds.length > 0) {
-      // Loop through each embed in the message
-      message.embeds.forEach(embed => {
-          // Access properties of the embed
-          const names = ['Unscramble event', 'Math event', 'Trivia event', 'Retype event']
-          if (names.includes(embed.title)) {
-              console.log('Chat Event!!!', embed.title);
-              const scriptPath = 'notification.py'
-              const args = ['Chat Event Spawned!', embed.title]
-              runPythonScript(scriptPath, args);
+    message.embeds.forEach(embed => {
+
+      if (types.includes(embed.title)) {
+        console.log('Chat Event!!!', embed.title), '\n';
+        const lines = embed.description.split('\n');
+
+        //                        Math Sniper                        //
+
+        if (embed.title === types[0], lines[0] === mathl1) {
+          const result = mathfc(lines[1]);
+          if (typeof result !== 'number' || isNaN(result)) {
+            console.log('fail.');
+            return;
+          }
+          if (typeof result == 'number') {
+            console.log(lines[1] + ' = ' + result);
+            setTimeout(() => { message.channel.send((result + '')); }, 2420);
+          }
+
+          //                       Retype Sniper                           //
+
+        }
+        if (embed.title === types[1]) {
+          if (lines[0].includes(retypel1)) {
+            const matches = lines[0].match(nump);
+            if (matches) {
+              const time = parseInt(matches[0], 10);
+              const result = retypefc(lines[1]);
+              console.log(lines[1] + ' = ' + result);
+              setTimeout(() => { message.channel.send((result + '')); }, ((time/15)*1000));
+            } 
+            else {
+              console.log("fail.");
+            }
 
           }
-      });
+
+
+        }
+      }
+    });
   }
 });
-
-////////////////////////// stats command ///////////////////////////////////
-
-client.on('messageCreate', (message) => {
-    // Check if the message starts with the prefix and is not sent by a bot
-    if (!message.content.startsWith(prefix) || message.author.bot ) return;
-
-    // Extract the command and arguments from the message content
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const command = args.shift().toLowerCase();
-
-    // Check if the command is 'bw'
-    if (command === 'bw', message.author.id === EpicPichu) {
-        console.log('loading...')
-        // Extract the Python script filename and arguments from the message
-        const filepath = path.resolve("D:/Works/Epic Stats/image.py");
-        const outputpath = path.resolve("D:/Works/Epic Stats/output.png");
-        const pythonArgs = args;
-
-        // Execute the Python script with the provided arguments
-        exec(`python "${filepath}" ${pythonArgs.join(' ')}`, (error, stdout, stderr) => {
-            if (error) {
-                console.log(`Error executing Python script: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                console.log(`Python script error output:\n${stderr}`);
-                return;
-            }
-            if (stdout.trim()==='error') {
-                message.channel.send('Invalid Player!');
-                console.log('Fail.')
-                return;
-            }
-            const attachment = new MessageAttachment(outputpath);
-            message.channel.send({ content: ' ', files:[attachment]});
-            console.log('Done.')
-        });
-    }
-});
-
-
-
-
-
 
 
 
@@ -112,4 +100,4 @@ client.login(token);
 
 
 
-//  ily_pichu >w< //s
+//  ily_pichu >w< //
