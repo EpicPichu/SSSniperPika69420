9ac878b685751e36e7a2269088a482630607c04c
EpicPichu <sakibhasan8024@gmail.com>
2024-03-03 06:29:26 +0600

    👍

--- notification.py
+++ notification.py
@@ -5,19 +5,19 @@ if len(sys.argv) == 3:  # Expecting the script name and two arguments
     arg1 = sys.argv[1]
     arg2 = sys.argv[2]
 
-def show_notification(title, message, icon_path):
+def show_notification(title='Chat Event!', message='chat event spawned!', icon_path="C:/Users/EpicPichu/Desktop/SSSniperPika69420/assets/pika.ico"):
     notification.notify(
         title=title,
         message=message,
         app_icon=icon_path,
         timeout=4
     )
-    winsound.PlaySound('assets/lights_on.wav', 0)
+    winsound.PlaySound('C:/Users/EpicPichu/Desktop/SSSniperPika69420/assets/lights_on.wav', 0)
 
 
 icon_path = "assets/pika.ico"
 
-show_notification(arg1, arg2, icon_path)
+show_notification()
 
 
 # ily_pichu >//< #
