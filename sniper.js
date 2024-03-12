ceecdf966cf273810d3c158b3153d536d1a32af0
EpicPichu <sakibhasan8024@gmail.com>
2024-03-12 23:04:22 +0600

    new stuff

--- sniper.js
+++ sniper.js
@@ -32,6 +32,17 @@ function retypefc(str) {
   return p1.replace(/\s(?=\w)|(?<=\w)\s/g, '').replace(/ {2,}/g, ' ');
 }
 
+function triviafc(str){
+  const str1 = str.replace(/['"]/g, '');
+  return str1.substring(4, str1.length - 2)
+}
+
+function unscfc(str) {
+  const p1 = str.substring(4, str.length - 2)
+  return p1.replace(/\s/g, '');
+}
+
+
 ////////////////////// constants ////////////////////////////
 
 
@@ -41,19 +52,19 @@ const EpicPichu = '598085365815050241';
 const pikachat = '234731357903781888';
 const pikabot = '601177246627135507';
 const types = ['Math event', 'Retype event', 'Trivia event', 'Unscramble event'];
-const mathl1 = 'seconds to solve the given riddle:';
-const retypel1 = "seconds to retype the following sentence:";
-const trivial1 = "seconds to answer the following question:";
+const mathl0 = 'seconds to solve the given riddle:';
+const retypel0 = "seconds to retype the following sentence:";
+const trivial0 = "seconds to answer the following question:";
+const unscl0 = "seconds to unscramble the given set of letters:";
 const nump = /\d+/;
 
 
 
 
-
-//////////////////////////// chat event sniper //////////////////////////////////////
+//////////////////////////// msg logger //////////////////////////////////////
 
 client.on('messageCreate', (message) => {
-  if (message.channelId === pikachat, message.author.id === pikabot) {
+  if (message.channelId === pikachat && message.author.id === pikabot) {
     console.log(message.author.username+': '+message.content);
     if (message.embeds.length>0) {
       message.embeds.forEach(embed => {
@@ -70,6 +81,8 @@ client.on('messageCreate', (message) => {
     }
   }
 });
+
+//////////////////////////// chat event sniper //////////////////////////////////////
 
 client.on('messageCreate', (message) => {
   if (message.embeds.length > 0) {
@@ -90,7 +103,7 @@ client.on('messageCreate', (message) => {
 
         //                        Math Sniper                        //
 
-        if (embed.title === types[0], (lines[0].includes(mathl1))) {
+        if (embed.title === types[0] && (lines[0].includes(mathl0))) {
           const result = mathfc(lines[1]);
           if (typeof result !== 'number' || isNaN(result)) {
             return;
@@ -103,20 +116,29 @@ client.on('messageCreate', (message) => {
 
         //                       Retype Sniper                           //
 
-        if (embed.title === types[1], lines[0].includes(retypel1)) {
+        if (embed.title === types[1] && lines[0].includes(retypel0)) {
           const result = retypefc(lines[1]);
           const matches = lines[0].match(nump);
-          setTimeout(() => { message.channel.send((result + '')); }, ((matches*1000)/6.5));
+          setTimeout(() => { message.channel.send((result + '')); }, ((matches*1000)/8));
           console.log(lines[1] + ' = ' + result);
         }
 
 
         //                      Trivia Sniper                           //
 
-        if (embed.title === types[2], lines[0].includes(trivial1)) {
+        if (embed.title === types[2] && lines[0].includes(trivial0)) {
           console.log(embed.description);
           import('open').then(open => {
-            open.default('https://www.google.com/search?q='+lines[1]);
+            open.default('https://www.google.com/search?q='+triviafc(lines[1]));
+          });
+        }
+        
+        //                      Unscramble Sniper                           //
+
+        if (embed.title === types[3] && lines[0].includes(unscl0)) {
+          console.log(embed.description);
+          import('open').then(open => {
+            open.default('https://www.unscramble.me/'+unscfc(lines[1]));
           });
         }
       }
